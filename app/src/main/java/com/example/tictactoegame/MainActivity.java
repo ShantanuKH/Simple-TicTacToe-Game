becadd4894255eeb3737a2d86c615d5f374a37b0
ShantanuKH <khadseshantanu02@gmail.com>
2024-06-11 08:21:56 +0530

    Set Button Onclick

--- app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.tictactoegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1, btn2, btn3 ,btn4 ,btn5, btn6, btn7, btn8, btn9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+//        This is how make method...Type this or Just type new V in parenthesis and click Enter
+
+//       We have to make method below, Structure should be same, and also this setOnclick will be override by the method we create
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+    private void init(){
+
+//        Find all buttons id
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+    }
+
+// This method which we create will override the above setOnclick method
+    public void Check(View view){
+
     }
 }
