ddc76a0e6a7cee9953141366752cf702c2862384
ShantanuKH <khadseshantanu02@gmail.com>
2024-06-11 08:34:33 +0530

    Build "x", "O" logic

--- app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button btn1, btn2, btn3 ,btn4 ,btn5, btn6, btn7, btn8, btn9;
+    int flag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +45,20 @@ public class MainActivity extends AppCompatActivity {
 
 // This method which we create will override the above setOnclick method
     public void Check(View view){
+//    view will also provide the reference of the button, we can give eny name to v in the the above code as here we gave v=view
+    Button btnCurrent = (Button)view;
+//    Here we are typecasting Button (right hand side) as for now we don't know that which button is pressed by typecasting the view will store the value of the button that is clicked in the btnCurrent.
+//    We know this as in the activity_main.xml we have given onClick attribute click so this above Click function will be called and the reference of the button that is clicked will be stored here in view.
+//    As now we are given type caste as button it is correct, it should always be kept in mind that whenever we type-caste anything then the view should be the same as of the class
+        if(flag == 0){
+            btnCurrent.setText("X");
+            flag=1;
+        }else{
+            btnCurrent.setText("O");
+            flag=0;
+        }
+
+
 
     }
 }
